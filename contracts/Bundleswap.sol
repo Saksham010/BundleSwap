3585fffaa0c04cf041b11a10d729da5fa0d61bef
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2023-04-19 10:01:46 +0545

    Added swap pooling functionality

--- contracts/Bundleswap.sol
+++ contracts/Bundleswap.sol
@@ -1,46 +1,97 @@
 pragma solidity 0.8.8;
 
+interface IERC20{
+    transferFrom(address sender, address recipient, uint256 amount)external returns(bool);
+}
+
 contract BundleSwap{
 
-    // Swap pool
+    // Pool id
+    uint256 internal counter;
 
-
-    // Token0 => Token1 => AggregationTime
-    mapping(address=>mapping(address => uint)) pool;
-
+    // Create a pool
     struct poolDetail{
         uint256 id;
-        address tokenA;
-        address tokenB;
+        uint256 aggregationTime;
+        uint256 creationTime;
     }
+
+    // Token0 => Token1 => AggregationTime
+    mapping(address=>mapping(address => poolDetail)) pool;
+
+
+    // Store user balances
+    struct deposits{
+        uint256 token0amount;
+        uint256 token1amount;
+    }
+
+    // User deposits (User -> pool id)
+    mapping(address => mapping (uint256 => deposits)) balances; 
+
+
+    // Events
+    event poolCreated(address,address,uint256);
 
     // Create a new pool
     function createSwapPool(address tokenA, address tokenB,uint256 aggregationTime) internal{
         // Create pool
-        pool[tokenA][tokenB] = aggregationTime;
+        pool[tokenA][tokenB] = poolDetail(counter,aggregationTime,block.timestamp);
+        //Increment counter
+        counter++;
+
+        // Emit event
+        emit poolCreated(tokenA,tokenB,aggregationTime);
     }
 
-    // Register swap request
-    function registerSwap(address tokenA, address tokenB, uint256 aggregationTime) public{
+    // Register swap request (TokenA/TokenB)
+    function registerSwap(address tokenA, address tokenB, uint256 amountA, uint256 amountB, uint256 aggregationTime) public{
 
         // Sort tokens
         (address _token0, address _token1) = sort(tokenA,tokenB);
+        // Sort amount
+        (uint256 _amount0,uint256 _amount1) = (tokenA == _token0)? (amountA,amountB): (amountB,amountA);
 
         // Check if the pool exists or not
-        if(pool[_token0][_token1]== 0){
+        if(pool[_token0][_token1].creationTime == 0){
             // If does not exist create a pool
             createSwapPool(_token0,_token1,aggregationTime);
 
             // Pool balance store
+            uint256 poolId = pool[_token0][_token1].id;
+            balances[msg.sender][poolId] = deposits(_amount0,_amount1);
+
+            // Transfer Token A and Token B to the contract
+            bool safe = IERC20(_token0).transferFrom(msg.sender,address(this),_amount0);
+            require(safe == true,"Token0 transfer failed");
+            safe = IERC20(_token1).transferFrom(msg.sender,address(this),_amount1);
+            require(safe == true,"Token0 transfer failed");
+
 
         } 
         else{
             // If the pool already exists
 
-        }
+            // Check aggregation time
+            uint256 timeElapsed = block.timestamp - pool[_token0][_token1].creationTime;
+            require(timeElapsed < pool[_token0][_token1].aggregationTime, "Cannot deposit after pool time expired. Create a new pool");
 
+            // Pool balance store
+            uint256 poolId = pool[_token0][_token1].id;
+            balances[msg.sender][poolId] = deposits(_amount0,_amount1);
+
+            // Transfer Token A and Token B to the contract
+            bool safe = IERC20(_token0).transferFrom(msg.sender,address(this),_amount0);
+            require(safe == true,"Token0 transfer failed");
+            safe = IERC20(_token1).transferFrom(msg.sender,address(this),_amount1);
+            require(safe == true,"Token0 transfer failed");
+        }
     }
 
+    // Swap with Uniswap V2 router
+    function bundleswap(address tokenA, address tokenB) public {
+
+    }
 
 
     // Sort token 
